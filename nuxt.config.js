fb7f26a750e6aa13c4a2906b6689e157f2712176
shashkaV <shashkav96@gmail.com>
2023-03-02 22:09:51 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
     linkActiveClass: 'custom-active-link',
     linkExactActiveClass: 'custom-exact-active-link',
     router: {
+      mode: 'hash',
       base: '/shashkaV/'
     }
   }
