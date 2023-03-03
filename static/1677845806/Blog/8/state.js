77b066aebb09570b7f5fe638569e240e2c64c54c
shashkaV <shashkav96@gmail.com>
2023-03-03 15:18:23 +0300

    f9e2658

---
+++ static/1677845806/Blog/8/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fshashkav.com\u002Fstatic\u002F1677845806",layout:"BasePageLayout",error:null,serverRendered:true,routePath:"\u002Fblog\u002F8",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fshashkav.com\u002F"}}}}("\u002F"));
---
+++ static/1677845806/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fshashkav.com\u002Fstatic\u002F1677845806",layout:"BasePageLayout",error:null,serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fshashkav.com\u002F"}}}}("\u002F"));
